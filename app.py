0699a5cc18c41528c16451d745f13c7d2cd79503
Ever Uriel Garcia <urielcookies@outlook.com>
2025-05-30 17:29:22 -0600

    CORS

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from apps.Test import test_bp
 from apps.EversVozAPI import eversvoz_bp
 from apps.PortfolioForm import portfoliocontactform_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 app.register_blueprint(test_bp)
 app.register_blueprint(eversvoz_bp)
