5de0288426dacfbe94ac052cb0e1be8ea0c1ebee
Ever Uriel Garcia <urielcookies@outlook.com>
2026-02-25 20:52:22 -0700

    Added space line

--- apps/EverApply/routes.py
+++ apps/EverApply/routes.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from apps.EverApply import everapply_bp
 
+
 @everapply_bp.route('/hello-world', methods=['GET'])
 def hello_world():
     return jsonify({"message": "EverApply is live!"}), 200
