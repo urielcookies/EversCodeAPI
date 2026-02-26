6945c4ec539880b1b43f42f3a25e50ec9866c820
Ever Uriel Garcia <urielcookies@outlook.com>
2026-02-25 21:04:14 -0700

    remove line

--- apps/EverApply/routes.py
+++ apps/EverApply/routes.py
@@ -1,7 +1,6 @@
 from flask import jsonify
 from apps.EverApply import everapply_bp
 
-
 @everapply_bp.route('/hello-world', methods=['GET'])
 def hello_world():
     return jsonify({"message": "EverApply is live!"}), 200
